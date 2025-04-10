34676cecb86f1b8292f41bc63722a59f20b58f8d
Andrew Kleindolph <akleindolph@gmail.com>
2025-04-10 06:15:20 -0700

    Create secrets_two_locations.py

---
+++ secrets_two_locations.py
@@ -0,0 +1,11 @@
+secrets = {
+    'wifi': [
+        {'ssid': 'StudentNet', 'password': ''}, #add the Studentnet password here
+        {'ssid': 'HomeWiFi', 'password': 'supersecretpassword'} #add your home WIFI here
+    ],
+    'timezone': "America/Los_Angeles",  # http://worldtimeapi.org/timezones
+    'aio_username': 'my_adafruit_io_username',
+    'aio_key': 'my_adafruit_io_key',
+    'broker': 'io.adafruit.com',
+    'port': 1883
+}
